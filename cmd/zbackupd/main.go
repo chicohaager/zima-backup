a8c02b3e455b582d3e911bb624ee965741199690
Holger Kuehn <holger.kuehn@virtual-services.info>
2026-09-22 12:14:15 +0200

    0.3.0-dev2

--- cmd/zbackupd/main.go
+++ cmd/zbackupd/main.go
@@ -34,7 +34,7 @@ import (
 )
 
 const (
-	version     = "0.3.0-dev1"
+	version     = "0.3.0-dev2"
 	serviceName = "zbackup"
 	binaryPath  = "/usr/bin/zbackupd"
 	dataPath    = "/DATA/AppData/zbackup"
